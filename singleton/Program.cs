53705a64b3c62a6f1260e44542bd325944195f69
Ahmed <ahmedelsharawy117@gmail.com>
2024-10-24 00:47:41 +0300

    add singleton design pattern

---
+++ singleton/Program.cs
@@ -0,0 +1,40 @@
+﻿// Singleton Design Pattern
+
+using System;
+
+class Singleton
+{
+    private static Singleton _instance;
+    private Singleton()
+    {
+    }
+
+    public static Singleton GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Singleton();
+        }
+
+        return _instance;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Singleton s1 = Singleton.GetInstance();
+        Singleton s2 = Singleton.GetInstance();
+
+        if (s1 == s2)
+        {
+            Console.WriteLine("Singleton works");
+        }
+        else
+        {
+            Console.WriteLine("Singleton doesn't work");
+        }
+    }
+    
+}
