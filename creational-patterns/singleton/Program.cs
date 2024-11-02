c1f6e6fb6bd7ff69eb7ec11ea2af295e47ccc7cf
Ahmed <ahmedelsharawy117@gmail.com>
2024-11-02 02:33:42 +0200

    chore: formatting

--- creational-patterns/singleton/Program.cs
+++ creational-patterns/singleton/Program.cs
@@ -2,39 +2,28 @@
 
 using System;
 
-class Singleton
-{
-    private static Singleton _instance;
-    private Singleton()
-    {
+class Singleton {
+  private static Singleton _instance;
+  private Singleton() {}
+
+  public static Singleton GetInstance() {
+    if (_instance == null) {
+      _instance = new Singleton();
     }
 
-    public static Singleton GetInstance()
-    {
-        if (_instance == null)
-        {
-            _instance = new Singleton();
-        }
-
-        return _instance;
-    }
+    return _instance;
+  }
 }
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Singleton s1 = Singleton.GetInstance();
-        Singleton s2 = Singleton.GetInstance();
+class Program {
+  public static void Main(string[] args) {
+    Singleton s1 = Singleton.GetInstance();
+    Singleton s2 = Singleton.GetInstance();
 
-        if (s1 == s2)
-        {
-            Console.WriteLine("Singleton works");
-        }
-        else
-        {
-            Console.WriteLine("Singleton doesn't work");
-        }
+    if (s1 == s2) {
+      Console.WriteLine("Singleton works");
+    } else {
+      Console.WriteLine("Singleton doesn't work");
     }
-    
+  }
 }
